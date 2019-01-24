0b978d74ef52517887fe6d963fad4bbfb341395f
LordGoblin <maxime78600@gmail.com>
2019-01-25 00:40:58 +0100

    ok

--- morpion/app.rb
+++ morpion/app.rb
@@ -4,22 +4,4 @@ Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'application.rb'
 
-#author = "max"
-#content = "blabla"
-
-#my_gossip = Gossip.new("max", "blabla")
-#puts "lol"
-#my_gossip.save
-
-
 Application.new.perform
-
-
-
-
-
-#require 'scrapper.rb'
-#allVille = Ville.new
-#puts allVille.mairie
-
-
